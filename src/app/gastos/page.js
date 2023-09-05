6b0f76831adfebe9a52ad163f3f5a11b877c5fc4
Ingridviieria <rm95396@fiap.com.br>
2023-09-05 10:59:34 -0300

    atualizado

--- src/app/gastos/page.js
+++ src/app/gastos/page.js
@@ -1,16 +1,22 @@
-//pagina para ver gastos
 import NavBar from "@/components/NavBar";
+import DataRow from "./DataRow";
 
-export default function Gastos() {
+async function getGastos(){
+    const url = "http://localhost:8080/api/v1/gastos"
+    const resp = await fetch(url, { next: { revalidate: 0 } })
+    return resp.json()
+}
+
+export default async function Gastos() {
+    const gastos = await getGastos()
     return (
     <>
         <NavBar active={"gastos"} />
-        <h1 style={{ fontSize: "120%", textAlign: "center" }}>
-            Gastos dos Pets
-        </h1>
-
-        <main className=" bg-gray-400 m-20 p-12">
-            <h2>Kira</h2>
+        <main className="bg-slate-900 mt-10 p-9 rounded max-w-xl m-auto">
+        <h2 className="text-slate-100 text-2xl">Gastos do animal</h2>
+        <div id="data">
+            {gastos.map(gastos => <DataRow key={gastos.id} gastos={gastos} />)}
+        </div>
         </main>
     </>
 
